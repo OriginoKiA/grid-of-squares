396b96c8d73acf28a7847d94c862cce6ffb644fd
Rene <cheesus928@gmail.com>
2016-06-01 20:07:07 -0500

    Added some code but the code for changing the grid size doesn't work for some reason

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {
-    var main = $(".main");
-    
-    for (var i=0;i<256;i++){
-        main.append("<div></div>");
+function generate(main,rows,columns) {    //generates the grid
+   for (var i=0;i<rows;i++){ //generate rows
+        main.append("<tr></tr>");
     }
-    $(".main div").mouseenter(function(){
-        $(this).css("background-color","yellow");
-    })
+    for (var i=0;i<columns;i++){ //generate columns
+        $("tr").append("<td></td>");
+    }
+}
+function getData(main) {
+        rows = $("#rows").val();
+        columns = $("#columns").val();
+        generate(main,rows,columns); //generate grid
+    }
+
+$(document).ready(function() {
+    var main = $(".main"); //refers to the table
+    var popup = $("#popup");
+    
+    getData(main);
+    
+    $(".main td").mouseenter(function(){
+        $(this).css("background-color","black");
+    });
+    popup.dialog({autoOpen: false});
+    
+    $("#changegrid").click(function(){
+        popup.dialog("open");
+    });
+    
+    $("#getdata").click(function() {
+        getData(main);
+    });
 });
+
